ccec90f97703e6401c4e283c85012e5597371a8c
SAGAR MUKHI <mukhisagar92@gmail.com>
2026-04-09 03:16:27 +0530

    Add About component with dark mode toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -7,9 +8,9 @@ function App() {
     <>
   <Navbar />
   <div className="container my-3">
-    <TextForm heading="Enter the text to analyze"/>
-  </div> 
- 
+    {/* <TextForm heading="Enter the text to analyze"/> */}
+    <About />
+  </div>  
    </>
   );
 }
